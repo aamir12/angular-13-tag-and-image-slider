8c2e3a277eab499d63d8659c24a001353572b79f
aamir12 <aamirkhan8878@gmail.com>
2023-09-19 16:16:44 +0530

    update

--- src/app/image-slider/image-slider.component.ts
+++ src/app/image-slider/image-slider.component.ts
@@ -6,7 +6,8 @@ import {
   Input,
   ViewChildren,
   QueryList,
-  HostListener,
+  SimpleChanges,
+  OnChanges,
 } from '@angular/core';
 
 @Component({
@@ -14,8 +15,8 @@ import {
   templateUrl: './image-slider.component.html',
   styleUrls: ['./image-slider.component.scss'],
 })
-export class ImageSliderComponent implements AfterViewInit {
-  @ViewChild('tabsList') tabsList!: ElementRef;
+export class ImageSliderComponent implements AfterViewInit,OnChanges {
+  @ViewChild('sliderList') sliderList!: ElementRef;
   @ViewChildren('sliderElements') sliderElements!: QueryList<ElementRef>;
   @Input('data') data: any[] = [];
 
@@ -33,6 +34,14 @@ export class ImageSliderComponent implements AfterViewInit {
     }, 0);
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes['data']) {
+      setTimeout(() => {
+        this.manageIcons();
+      },0) 
+    }
+  }
+
   selectCard(index: number): void {
     if(this.data.length === 0) {
       return;
@@ -42,8 +51,8 @@ export class ImageSliderComponent implements AfterViewInit {
     const selectedTag = this.sliderElements.filter((_, i) => i === index)[0]
       .nativeElement;
 
-    if (this.tabsList && selectedTag) {
-      const container = this.tabsList.nativeElement;
+    if (this.sliderList && selectedTag) {
+      const container = this.sliderList.nativeElement;
 
       const containerWidth = container.offsetWidth;
       const selectedTagWidth = selectedTag.offsetWidth;
@@ -53,13 +62,13 @@ export class ImageSliderComponent implements AfterViewInit {
       const centerOffset = (containerWidth - selectedTagWidth) / 2;
 
       if (selectedTagOffsetLeft - 50 < containerScrollLeft) {
-        this.tabsList.nativeElement.scrollLeft =
+        this.sliderList.nativeElement.scrollLeft =
           selectedTagOffsetLeft - centerOffset;
       } else if (
         selectedTagOffsetLeft + selectedTagWidth + 50 >
         containerScrollLeft + containerWidth
       ) {
-        this.tabsList.nativeElement.scrollLeft =
+        this.sliderList.nativeElement.scrollLeft =
           selectedTagOffsetLeft +
           selectedTagWidth -
           containerWidth +
@@ -69,12 +78,12 @@ export class ImageSliderComponent implements AfterViewInit {
   }
 
   scrollRight(): void {
-    this.tabsList.nativeElement.scrollLeft += 200;
+    this.sliderList.nativeElement.scrollLeft += 200;
     this.manageIcons();
   }
 
   scrollLeft(): void {
-    this.tabsList.nativeElement.scrollLeft -= 200;
+    this.sliderList.nativeElement.scrollLeft -= 200;
     this.manageIcons();
   }
 
@@ -83,10 +92,10 @@ export class ImageSliderComponent implements AfterViewInit {
       return;
     }
 
-    const scrollLeft = this.tabsList.nativeElement.scrollLeft;
+    const scrollLeft = this.sliderList.nativeElement.scrollLeft;
     const maxScrollValue =
-      this.tabsList.nativeElement.scrollWidth -
-      this.tabsList.nativeElement.clientWidth -
+      this.sliderList.nativeElement.scrollWidth -
+      this.sliderList.nativeElement.clientWidth -
       20;
 
     if (scrollLeft >= 20) {
--- src/app/tag-slider/tag-slider.component.ts
+++ src/app/tag-slider/tag-slider.component.ts
@@ -1,48 +1,54 @@
-import {
-  Component,
-  ElementRef,
-  ViewChild,
-  AfterViewInit,
-  Output,
-  EventEmitter,
-  Input,
-  ViewChildren,
-  QueryList,
-  HostListener,
-} from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnInit, Output, EventEmitter, Input, ViewChildren, QueryList, HostListener, SimpleChanges, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-tag-slider',
   templateUrl: './tag-slider.component.html',
-  styleUrls: ['./tag-slider.component.scss'],
+  styleUrls: ['./tag-slider.component.scss']
 })
-export class TagSliderComponent implements AfterViewInit {
+export class TagSliderComponent implements AfterViewInit,OnChanges {
   @ViewChild('tabsList') tabsList!: ElementRef;
   @ViewChildren('tagElement') tagElements!: QueryList<ElementRef>;
 
   showLeftArrow = false;
   showRightArrow = false;
 
-  dragging = false;
-  isDragging = false;
-  @Input('tags') tags: string[] = [];
+  @Input('tags') tags:string[] = [];
+  @Input('selectedTag') selectedTag:string = '';
   @Output('onSelect') onSelect = new EventEmitter<string>();
-
-  constructor(private el: ElementRef) {}
+  
+  constructor(private el: ElementRef) { }
 
   activeTabIndex: number = 0;
 
   ngAfterViewInit(): void {
     setTimeout(() => {
-      this.manageIcons();
-    }, 0);
+        this.manageIcons();
+        if(this.selectedTag) {
+            this.selectTag(this.selectedTag);
+        }
+    },0)
   }
 
-  selectTab(index: number, tag: string): void {
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+    if(changes['tags']) {
+      setTimeout(() => {
+        this.manageIcons();
+        this.selectTag(this.selectedTag);
+      },0)
+      
+    }
+  }
+
+  selectTag(tag:string): void {
+
+    if( this.tags.length === 0) {
+      return;
+    }
+    const index = this.tags.findIndex(x => x === tag);
     this.activeTabIndex = index;
     this.onSelect.emit(tag);
-    const selectedTag = this.tagElements.filter((_, i) => i === index)[0]
-      .nativeElement;
+    const selectedTag = this.tagElements.filter((_,i)=> i === index)[0].nativeElement;
 
     if (this.tabsList && selectedTag) {
       const container = this.tabsList.nativeElement;
@@ -54,24 +60,16 @@ export class TagSliderComponent implements AfterViewInit {
       const containerScrollLeft = container.scrollLeft;
       const centerOffset = (containerWidth - selectedTagWidth) / 2;
 
-      if (selectedTagOffsetLeft - 50 < containerScrollLeft) {
-        this.tabsList.nativeElement.scrollLeft =
-          selectedTagOffsetLeft - centerOffset;
-      } else if (
-        selectedTagOffsetLeft + selectedTagWidth + 50 >
-        containerScrollLeft + containerWidth
-      ) {
-        this.tabsList.nativeElement.scrollLeft =
-          selectedTagOffsetLeft +
-          selectedTagWidth -
-          containerWidth +
-          centerOffset;
+      if (((selectedTagOffsetLeft-50) < containerScrollLeft)) {
+        this.tabsList.nativeElement.scrollLeft = selectedTagOffsetLeft - centerOffset;
+      } else if (selectedTagOffsetLeft + selectedTagWidth + 50 > containerScrollLeft + containerWidth) {
+        this.tabsList.nativeElement.scrollLeft = selectedTagOffsetLeft + selectedTagWidth - containerWidth + centerOffset;
       }
     }
   }
 
   scrollRight(): void {
-    this.tabsList.nativeElement.scrollLeft += 200;
+    this.tabsList.nativeElement.scrollLeft += 200; 
     this.manageIcons();
   }
 
@@ -81,15 +79,12 @@ export class TagSliderComponent implements AfterViewInit {
   }
 
   manageIcons(): void {
-    if (!this.tabsList || this.tags.length === 0) {
+    if(this.tags.length === 0) { 
       return;
     }
 
     const scrollLeft = this.tabsList.nativeElement.scrollLeft;
-    const maxScrollValue =
-      this.tabsList.nativeElement.scrollWidth -
-      this.tabsList.nativeElement.clientWidth -
-      20;
+    const maxScrollValue = this.tabsList.nativeElement.scrollWidth - this.tabsList.nativeElement.clientWidth - 20;
 
     if (scrollLeft >= 20) {
       this.showLeftArrow = true;
@@ -104,21 +99,7 @@ export class TagSliderComponent implements AfterViewInit {
     }
   }
 
-  onMousemove(event: MouseEvent) {
-    if (!this.dragging) return;
-    this.isDragging = true;
-    this.tabsList.nativeElement.scrollLeft -= event.movementX;
-  }
+  
 
-  onMousedown(event: MouseEvent) {
-    this.dragging = true;
-  }
-
-  @HostListener('document:mouseup', ['$event'])
-  onMouseUp(event: MouseEvent) {
-    if (this.el.nativeElement.contains(event.target)) {
-      this.dragging = false;
-      this.isDragging = false;
-    }
-  }
 }
+
